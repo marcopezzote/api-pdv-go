1052b8de8b01f7a1a55fe3c6254b61a0d2e6308a
marcopezzote <pezzote@outlook.com>
2024-07-11 01:21:15 -0300

    "Fix prepared statement cache issue"

--- config/database.go
+++ config/database.go
@@ -18,12 +18,22 @@ func ConnectDatabase() {
     dbname := os.Getenv("DB_NAME")
     port := os.Getenv("DB_PORT")
 
-    dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=require", host, user, password, dbname, port)
+    dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=require statement_cache_mode=describe", host, user, password, dbname, port)
     database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
     if err != nil {
         log.Fatal("Failed to connect to database:", err)
     }
+
+    sqlDB, err := database.DB()
+    if err != nil {
+        log.Fatal("Failed to get database handle:", err)
+    }
+
+    // Configurar o pool de conexões
+    sqlDB.SetMaxIdleConns(10)
+    sqlDB.SetMaxOpenConns(100)
+    sqlDB.SetConnMaxLifetime(0)
 
     DB = database
 }
