e8df05f86c1c04b36de3411162c3c55ae41d6fbe
marcopezzote <pezzote@outlook.com>
2024-07-11 01:21:45 -0300

    Ajuste para retornar status 201 ao criar usuario

--- handlers/usuario.go
+++ handlers/usuario.go
@@ -5,6 +5,7 @@ import (
 	"api-pdv-golang/models"
 	"encoding/json"
 	"net/http"
+	"strings"
 
 	"golang.org/x/crypto/bcrypt"
 )
@@ -19,13 +20,29 @@ func CreateUsuario(w http.ResponseWriter, r *http.Request) {
 	var usuario models.Usuario
 	json.NewDecoder(r.Body).Decode(&usuario)
 
+	if strings.TrimSpace(usuario.Nome) == "" || strings.TrimSpace(usuario.Email) == "" || strings.TrimSpace(usuario.Senha) == "" {
+		http.Error(w, "Nome, email e senha são obrigatórios!", http.StatusBadRequest)
+		return
+	}
+
+	var existingUser models.Usuario
+	if err := config.DB.Where("email = ? ", usuario.Email).First(&existingUser).Error; err == nil {
+		http.Error(w, "Email already in use!", http.StatusBadRequest)
+		return
+	}
+
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(usuario.Senha), bcrypt.DefaultCost )
 	if err != nil {
 		http.Error(w, "Failed to hash password", http.StatusInternalServerError)
 		return
 	}
 	usuario.Senha = string(hashedPassword)
+
+	if err := config.DB.Create(&usuario).Error; err != nil {
+		http.Error(w, "Failed to create user", http.StatusInternalServerError)
+		return
+	}
 	
-	config.DB.Create(&usuario)
+	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(usuario)
 }
